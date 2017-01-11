98dc6f037b088956aea87921c42efdc4174d67a9
marvel80 <ankurext@gmail.com>
2017-01-10 20:06:17 -0800

    add maven dependencies and project structure

---
+++ membd/src/main/java/me/akka/app/App.java
@@ -0,0 +1,13 @@
+package me.akka.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
