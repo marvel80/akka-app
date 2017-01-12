e58f89f15e259878bfd59058c8169a76a8ad1331
marvel80 <ankurext@gmail.com>
2017-01-12 12:40:51 -0800

    check actor behavior using futures API

--- membd/src/test/java/me/akka/app/actor/MiActorTest.java
+++ membd/src/test/java/me/akka/app/actor/MiActorTest.java
@@ -1,24 +1,43 @@
 package me.akka.app.actor;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
-import akka.testkit.TestActorRef;
+import akka.pattern.Patterns;
+import scala.compat.java8.FutureConverters;
+import scala.concurrent.Future;
 
 public class MiActorTest {
+	//heavy, so recommended to create one per application
 	ActorSystem testSystem = ActorSystem.create();
 	
+	ActorRef testActRef = testSystem.actorOf(Props.create(MiActor.class));
+	
 	@Test
-	public void testMiActorForMI(){
-		TestActorRef<MiActor> tstActorRef = TestActorRef.create(testSystem, Props.create(MiActor.class));
-		MiActor actr = tstActorRef.underlyingActor();
+	public void testMiActorForMI() throws InterruptedException, ExecutionException, TimeoutException{
+		Future<Object> scalaFuture = Patterns.ask(testActRef, "Mission Impossible", 500);
+		final CompletionStage<Object> compStg = FutureConverters.toJava(scalaFuture);
+		final CompletableFuture<Object> javaFuture = (CompletableFuture<Object>) compStg;
 		
-		//tstActorRef.tell("Mission Impossible", ActorRef.noSender());
-		tstActorRef.receive("Mission Impossible", ActorRef.noSender());
+		Assert.assertEquals(String.valueOf(javaFuture.get(500, TimeUnit.MILLISECONDS)), "Tom Cruise");
+	}
+	
+	@Test(expected = Exception.class)
+	public void testMiActorForStarWars() throws InterruptedException, ExecutionException, TimeoutException{
+		Future<Object> scalaFuture = Patterns.ask(testActRef, "Star Wars", 500);
+		final CompletionStage<Object> compStg = FutureConverters.toJava(scalaFuture);
+		final CompletableFuture<Object> javaFuture = (CompletableFuture<Object>) compStg;
 		
-		
+		javaFuture.get(500, TimeUnit.MILLISECONDS);
 	}
 
 }
