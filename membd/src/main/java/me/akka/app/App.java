09dd72725bc3162364cbff557738b58ade82cdb3
marvel80 <ankurext@gmail.com>
2017-01-24 13:21:41 -0800

    memdb in cluster mode (local)

--- membd/src/main/java/me/akka/app/App.java
+++ membd/src/main/java/me/akka/app/App.java
@@ -1,13 +1,23 @@
 package me.akka.app;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import me.akka.app.actor.MemDbActor;
 
 public class App {
 	public static void main(String[] args) {
-		ActorSystem mainActorSys = ActorSystem.create("memDb");
-		mainActorSys.actorOf(Props.create(MemDbActor.class) , "memDbAct");
+		//get the default ports for initial cluster
+		String[] startupNodes = {"2551" , "2552", "25520"};	// as defined in application.conf , 2551 and 2552 are the seed nodes.
 		
+		for(String port : startupNodes){
+			//default config file is application.conf
+			Config portConfig = ConfigFactory.parseString("akka.remote.artery.canonical.port=" + port).withFallback(ConfigFactory.load());
+			
+			ActorSystem mainActorSys = ActorSystem.create("memDb" , portConfig);
+			mainActorSys.actorOf(Props.create(MemDbActor.class) , "memDbAct");	
+		}
 	}
 }
