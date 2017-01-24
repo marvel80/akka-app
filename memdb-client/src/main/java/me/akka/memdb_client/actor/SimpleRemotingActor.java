fdd6f2ad03b698a7276c8a94745ef9c46fb535a6
marvel80 <ankurext@gmail.com>
2017-01-23 17:55:20 -0800

    define supervision strategy

--- memdb-client/src/main/java/me/akka/memdb_client/actor/SimpleRemotingActor.java
+++ memdb-client/src/main/java/me/akka/memdb_client/actor/SimpleRemotingActor.java
@@ -8,9 +8,12 @@ import org.slf4j.LoggerFactory;
 import akka.actor.ActorIdentity;
 import akka.actor.ActorRef;
 import akka.actor.Identify;
+import akka.actor.OneForOneStrategy;
 import akka.actor.ReceiveTimeout;
+import akka.actor.SupervisorStrategy;
 import akka.actor.UntypedActor;
 import akka.japi.Procedure;
+import akka.japi.pf.DeciderBuilder;
 import me.akka.app.message.DbGetMessage;
 import me.akka.app.message.DbPutMessage;
 import scala.concurrent.duration.Duration;
@@ -20,12 +23,19 @@ public class SimpleRemotingActor extends UntypedActor {
 	private final String path;
 	private ActorRef memDb;
 	private static final Logger log = LoggerFactory.getLogger(SimpleRemotingActor.class);
+	private SupervisorStrategy strategy ;
 
 	public SimpleRemotingActor(String path) {
 		this.path = path;
 		memDb = null;
 
 		this.identify();
+
+		strategy = new OneForOneStrategy(10, Duration.create("1 minute"),
+				DeciderBuilder.match(ArithmeticException.class, e -> SupervisorStrategy.resume())
+						.match(NullPointerException.class, e -> SupervisorStrategy.restart())
+						.match(IllegalArgumentException.class, e -> SupervisorStrategy.stop())
+						.matchAny(x -> SupervisorStrategy.escalate()).build());
 	}
 
 	private void identify() {
@@ -76,5 +86,14 @@ public class SimpleRemotingActor extends UntypedActor {
 			}
 		}
 	};
-
+	
+	/**
+	 * defines a supervision strategy. OneForOneStrategy is applied since it is
+	 * applicable to just the failing child/children
+	 */
+	@Override
+	public SupervisorStrategy supervisorStrategy(){
+		return strategy;
+	}
+	
 }
