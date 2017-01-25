3627d489583e1ffbc9224b3451cbb6e074ea6a8c
marvel80 <ankurext@gmail.com>
2017-01-24 17:05:43 -0800

    actor subscription for cluster events. - test with MemberUp message

--- membd/src/main/java/me/akka/app/actor/MemDbActor.java
+++ membd/src/main/java/me/akka/app/actor/MemDbActor.java
@@ -8,6 +8,11 @@ import org.slf4j.LoggerFactory;
 
 import akka.actor.AbstractActor;
 import akka.actor.Status;
+import akka.cluster.Cluster;
+import akka.cluster.ClusterEvent;
+import akka.cluster.ClusterEvent.MemberEvent;
+import akka.cluster.ClusterEvent.MemberUp;
+import akka.cluster.ClusterEvent.UnreachableMember;
 import akka.japi.pf.ReceiveBuilder;
 import me.akka.app.exception.MissingKeyException;
 import me.akka.app.message.DbGetMessage;
@@ -17,7 +22,21 @@ import me.akka.app.message.DbPutMessage;
 public class MemDbActor extends AbstractActor {
 	private static final Logger log = LoggerFactory.getLogger(MemDbActor.class);
 	private final Map<String, String> map;
-
+	
+	Cluster clusterInfo = Cluster.get(getContext().system());
+	
+	@Override
+	public void preStart() {
+		log.info("subscribing to cluster events");
+		clusterInfo.subscribe(self(), ClusterEvent.initialStateAsEvents(), MemberEvent.class, UnreachableMember.class);
+	}
+	
+	@Override
+	public void postStop(){
+		log.info("un-subscribing from the cluster activity");
+		clusterInfo.unsubscribe(self());
+	}
+	
 	private MemDbActor() {
 		map = new HashMap<>();
 		receive(ReceiveBuilder.match(DbPutMessage.class, putMessage -> {
@@ -30,8 +49,10 @@ public class MemDbActor extends AbstractActor {
 			String val = map.get(getMessage.getKey());
 			sender().tell(val == null ? new Status.Failure(new MissingKeyException(getMessage.getKey()))
 					: new DbOperationResultMessage(getMessage.getKey(), val), self());
+		}).match(MemberUp.class, z -> {
+			log.info("member joined and is UP. member={}" , ((MemberUp)z).member());
 		}).matchAny(o -> {
-			log.info("recieved msg unkown", o);
+			log.info("recieved msg unkown. classname={}", o.getClass().getName());
 			sender().tell(new Status.Failure(new IllegalArgumentException("bad message")), self());
 		}).build());
 	}
